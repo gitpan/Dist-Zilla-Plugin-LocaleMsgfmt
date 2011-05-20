b67eb55660467f4979bbd7720857f0788ad77c52
Jerome Quelin <jquelin@cpan.org>
2011-05-20 13:01:18 +0000

    Import of JQUELIN/Dist-Zilla-Plugin-LocaleMsgfmt-1.203 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-LocaleMsgfmt
    gitpan-cpan-version:      1.203
    gitpan-cpan-path:         JQUELIN/Dist-Zilla-Plugin-LocaleMsgfmt-1.203.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/LocaleMsgfmt.pm
+++ lib/Dist/Zilla/Plugin/LocaleMsgfmt.pm
@@ -1,46 +1,68 @@
 package Dist::Zilla::Plugin::LocaleMsgfmt;
 BEGIN {
-  $Dist::Zilla::Plugin::LocaleMsgfmt::VERSION = '1.202';
+  $Dist::Zilla::Plugin::LocaleMsgfmt::VERSION = '1.203';
 }
-# ABSTRACT: Dist::Zilla plugin that compiles Local::Msgfmt .po files to .mo files
+# ABSTRACT: compiles .po files to .mo files with Local::Msgfmt
 
-
-use Locale::Msgfmt;
+use Locale::Msgfmt 0.14;
 use Moose;
+use MooseX::Has::Sugar;
+use Path::Class;
+
 with 'Dist::Zilla::Role::BeforeBuild';
 
 
+# -- attributes
+
+# For Config::MVP - specify setting names that may have multiple values and that will always
+# be stored in an arrayref
 sub mvp_multivalue_args { qw(locale) }
 
 
+
+has recursive => ( ro, isa=>'Bool', default=>1 );
 has locale => (
-    is      => 'ro',
+    ro, lazy, auto_deref,
     isa     => 'ArrayRef[Str]',
-    lazy    => 1,
     default => sub {
-        my ($self) = @_;
-        my $path = File::Spec->catfile( $self->zilla->root, 'share', 'locale' );
-        if ( -e $path ) {
-            return [$path];
-        }
-        else {
-            return [];
-        }
+        my $self = shift;
+        my $path = dir( $self->zilla->root, 'share', 'locale' );
+        return -e $path ? [ $path ] : [ ];
     },
 );
 
 
+# -- public methods
+
+#
+# to implement Dist::Zilla::Role::BeforeBuild
 sub before_build {
     my ( $self, $args ) = @_;
 
-    for my $dir ( @{ $self->locale } ) {
-        my $path = Path::Class::Dir->new($dir);
-        if ( -e $path ) {
-            $self->log("Generating .mo files from .po files in $path");
-            Locale::Msgfmt::msgfmt( { in => $path->absolute, verbose => 1, remove => 0 } );
-        }
-        else {
+    for my $dir ( $self->locale ) {
+        my $path = dir($dir);
+        if ( ! -e $path ) {
             warn "Skipping invalid path: $path";
+            next;
+        }
+
+        # find directories if recursive behaviour wanted
+        my @pathes;
+        if ( $self->recursive ) {
+            $path->recurse(
+                callback => sub {
+                    my $p = shift;
+                    push @pathes, $p if -d $p;
+                }
+            );
+        } else {
+            push @pathes, $path;
+        }
+
+        # generating mo files
+        foreach my $p ( @pathes ) {
+            $self->log("Generating .mo files from .po files in $p");
+            msgfmt( { in => $p->absolute, verbose => 1, remove => 0 } );
         }
     }
 }
@@ -50,39 +72,39 @@ no Moose;
 
 1;
 
-__END__
+
 =pod
 
 =head1 NAME
 
-Dist::Zilla::Plugin::LocaleMsgfmt - Dist::Zilla plugin that compiles Local::Msgfmt .po files to .mo files
+Dist::Zilla::Plugin::LocaleMsgfmt - compiles .po files to .mo files with Local::Msgfmt
 
 =head1 VERSION
 
-version 1.202
+version 1.203
 
 =head1 DESCRIPTION
 
-Put the following in your dist.ini
+Put the following in your S<F<dist.ini> :>
 
     [LocaleMsgfmt]
-    locale = share/locale ;; (optional)
+    locale = share/locale ; this is the default
 
 This plugin will compile all of the .po files it finds in the locale directory into .mo
 files, via Locale::Msgfmt.
 
 =head1 ATTRIBUTES
 
+=head2 recursive
+
+Whether to look up in the locale files recursively.
+
 =head2 locale
 
-=head1 METHODS
+Path to the directory containing the locale files.
 
-=head2 mvp_multivalue_args
-
-For Config::MVP - specify setting names that may have multiple values and that will always
-be stored in an arrayref
-
-=head2 before_build
+=for Pod::Coverage before_build
+    mvp_multivalue_args
 
 =head1 TODO
 
@@ -95,10 +117,13 @@ Patrick Donelan <pdonelan@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Patrick Donelan.
+This software is copyright (c) 2011 by Patrick Donelan.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
+
+
+__END__
 
